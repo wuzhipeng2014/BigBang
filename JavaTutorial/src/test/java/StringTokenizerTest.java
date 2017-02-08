7b8401efd637f1308ffd6020469977538ebb265d
zhipeng.wu <zhipeng.wu@qunar.com>
2017-02-08 16:41:11 +0800

    init

---
+++ JavaTutorial/src/test/java/StringTokenizerTest.java
@@ -0,0 +1,13 @@
+import java.util.StringTokenizer;
+
+/**
+ * Created by zhipengwu on 17-2-8.
+ */
+public class StringTokenizerTest {
+    public static void main(String[] args) {
+        StringTokenizer stringTokenizer=new StringTokenizer("this,is, a test");
+        while (stringTokenizer.hasMoreTokens()){
+            System.out.println(stringTokenizer.nextToken());
+        }
+    }
+}
