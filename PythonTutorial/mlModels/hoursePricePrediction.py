8141438b4b1481706568e2bdf857f30a80102798
zhipeng.wu <zhipeng.wu@qunar.com>
2017-06-19 11:42:49 +0800

    MOD:

--- PythonTutorial/mlModels/hoursePricePrediction.py
+++ PythonTutorial/mlModels/hoursePricePrediction.py
@@ -25,15 +25,16 @@ all_data = pd.concat((train.loc[:, 'MSSubClass':'SaleCondition'], test.loc[:, 'M
 
 ## 显示房价的柱状图
 matplotlib.rcParams['figure.figsize'] = (12.0, 6.0)
+# 创建dataframe
 prices = pd.DataFrame({"price": train["SalePrice"], "log(price+1)": np.log1p(train["SalePrice"])})
 prices.hist()
 plt.show()
 print '房价柱状图显示完成'
 
-##将房价的数据转换为对数形式
+##将房价数据进行对数处理
 train["SalePrice"] = np.log1p(train["SalePrice"])
 
-##将所有数字特征进行对数处理
+##将所有偏差较大的数字特征进行对数处理
 numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
 
 # 计算skewess,并对数字特征进行对数处理
@@ -55,7 +56,7 @@ y = train.SalePrice
 
 ##使用正则化线性回归拟合数据, 同时定义了一个函数返回交叉验证的rmse 误差,用来对模型进行评估并选出表现最好的参数
 ##------------------------------------------------------------------------------------
-# 使用正则化线性模型你和数据
+# 使用正则化线性模型拟合数据
 ##------------------------------------------------------------------------------------
 from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
 from sklearn.model_selection import cross_val_score
@@ -70,8 +71,8 @@ def rmse_cv(model):
 model_ridge = Ridge()
 
 alphas = [0.05, 0.1, 0.3, 1, 3, 5, 10, 15, 30, 50, 75]
-cv_ridge=[rmse_cv(Ridge(alpha=alpha)).mean()
-          for alpha in alphas]
+cv_ridge = [rmse_cv(Ridge(alpha=alpha)).mean()
+            for alpha in alphas]
 cv_ridge = pd.Series(cv_ridge, index=alphas)
 cv_ridge.plot(title="validation-just do it")
 plt.xlabel("alpha")
@@ -80,5 +81,35 @@ plt.show()
 
 print '最小的rmse为:'
 print cv_ridge.min()
+
+## lasso 模型
+model_lasso = LassoCV(alphas=[1, 0.1, 0.001, 0.0005]).fit(X_train, y)
+print "lasso rmse: "
+print rmse_cv(model_lasso).mean()
+
+coef = pd.Series(model_lasso.coef_, index=X_train.columns)
+
+print(
+"Lasso picked " + str(sum(coef != 0)) + " variables and eliminated the other " + str(sum(coef == 0)) + " variables")
+
+imp_coef = pd.concat([coef.sort_values().head(10), coef.sort_values().tail(10)])
+
+imp_coef.plot(kind="barh")
+plt.title("Coefficients in the Lasso Model")
+plt.show()
+
+# 模型预测结果展示
+preds = pd.DataFrame({"preds": model_lasso.predict(X_train), "true": y})
+preds["residuals"] = preds["true"] - preds["preds"]
+preds.plot(x="preds", y="residuals", kind="scatter")
+plt.show()
+
+## 显示估计误差的分布
+diff = preds["true"] - preds["preds"]
+diff.hist()
+plt.show()
+
+
+
 
 
