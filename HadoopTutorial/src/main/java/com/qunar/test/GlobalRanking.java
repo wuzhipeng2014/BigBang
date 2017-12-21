c74967f70086c6d37a1fbd88d033fe4df90686f7
zhipeng.wu <zhipeng.wu@qunar.com>
2017-12-21 17:24:25 +0800

    MOD:mapreduce全局排序

---
+++ HadoopTutorial/src/main/java/com/qunar/test/GlobalRanking.java
@@ -0,0 +1,115 @@
+package com.qunar.test;
+
+import com.qunar.MRCommon.MRCommonModel;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapred.JobPriority;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.partition.InputSampler;
+import org.apache.hadoop.mapreduce.lib.partition.TotalOrderPartitioner;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.elasticsearch.common.Strings;
+
+import java.io.IOException;
+
+/**
+ * Created by zhipengwu on 17-12-21. 利用mapreduce 实现全局排序
+ */
+public class GlobalRanking extends Configured implements Tool {
+    public static class MRCommonModelMapper extends Mapper<Object, Text, Text, Text> {
+        @Override
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            String line=value.toString();
+            if (!Strings.isNullOrEmpty(line)){
+                String[] split = line.split("\t");
+                if (split.length==3){
+                    if (!split[2].equals("null")) {
+                        Double similarity = Double.valueOf(split[2]);
+                        context.write(new Text(split[2]),new Text(line));
+                    }
+                }
+            }
+        }
+    }
+
+    public static class MRCommonModelReducer extends Reducer<Text, Text, NullWritable, Text> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context)
+                throws IOException, InterruptedException {
+            super.reduce(key, values, context);
+            for(Text value:values){
+                context.write(NullWritable.get(),new Text(value));
+            }
+        }
+    }
+
+    public int run(String[] args) throws Exception {
+        if (args.length != 4) {
+            System.err.println("./run <input> <output> <reducetasknumber> <paritionPath>");
+            System.exit(1);
+        }
+        String inputPaths = args[0];
+        String outputPath = args[1];
+        int numReduceTasks = Integer.parseInt(args[2]);
+        String paritionPath=args[3];
+
+        Configuration conf = this.getConf();
+        conf.set("mapred.job.queue.name", "wirelessdev");
+        conf.set("mapreduce.map.memory.mb", "8192");
+        conf.set("mapreduce.reduce.memory.mb", "8192");
+        conf.set("mapred.child.reduce.java.opts", "-Xmx8192m");
+        conf.set("mapreduce.reduce.java.opts", "-Xmx8192m");
+        conf.setBoolean("mapreduce.input.fileinputformat.input.dir.recursive", true);
+        conf.set("mapred.job.priority", JobPriority.VERY_HIGH.name());
+        conf.setBoolean("mapred.output.compress", true);
+        conf.setClass("mapred.output.compression.codec", GzipCodec.class, CompressionCodec.class);
+        // 传递变量
+        // conf.setStrings("target", targetKey);
+
+        Job job = Job.getInstance(conf);
+        job.setJobName("SuggestionList_ZhipengWu");
+        job.setJarByClass(GlobalRanking.class);
+        job.setMapperClass(MRCommonModelMapper.class);
+        job.setReducerClass(MRCommonModelReducer.class);
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
+
+        //todo 全局排序
+        TotalOrderPartitioner.setPartitionFile(job.getConfiguration(), new Path(paritionPath));
+        InputSampler.Sampler<Text, Text> sampler = new InputSampler.RandomSampler(0.01, 1000, 100);
+        InputSampler.writePartitionFile(job, sampler);
+        job.setPartitionerClass(TotalOrderPartitioner.class);
+
+       
+
+
+        FileInputFormat.setInputPaths(job, inputPaths);
+        FileOutputFormat.setOutputPath(job, new Path(outputPath));
+        job.setNumReduceTasks(numReduceTasks);
+
+        boolean success = job.waitForCompletion(true);
+        return success ? 0 : 1;
+    }
+
+    public static void main(String[] args) {
+        int status = 0;
+        try {
+            status = ToolRunner.run(new GlobalRanking(), args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.exit(status);
+    }
+}
--- SparkTutorial/src/main/scala/com/qunar/test/HelloTest.scala
+++ SparkTutorial/src/main/scala/com/qunar/test/HelloTest.scala
@@ -29,15 +29,17 @@ object HelloTest {
   def dotProduct(x: Map[String, Double], y: Map[String, Double]): Double = {
     var productResult = 0d
     for (elem <- x.keys) {
-      productResult+=(x.apply(elem)*y.get(elem).getOrElse(0d))
+      productResult += (x.apply(elem) * y.get(elem).getOrElse(0d))
     }
+//    println(x.values.toString()+"dotproduct="+productResult)
     productResult
   }
 
   // Compute norm of dict
   def norm(x: Map[String, Double]): Double = {
-    var modResult = 0
-    x.values.foreach(v=>modResult+=(modResult*modResult))
+    var modResult = 0d
+    x.values.foreach(v => modResult += (v * v))
+//    println(x.values.toString()+"norm="+modResult)
     math.sqrt(modResult)
   }
 
@@ -51,20 +53,29 @@ object HelloTest {
     val sc = new SparkContext(conf)
 
     // Parse input lines
-    val parsed = sc.textFile("/home/zhipengwu/secureCRT/postestinput.libsvm").map(parseLine)
-    val parsedNeg = sc.textFile("/home/zhipengwu/secureCRT/negtestinput.libsvm").map(parseLine)
+        val parsed = sc.textFile("/home/zhipengwu/secureCRT/postestinput.libsvm").map(parseLine)
+        val parsedNeg = sc.textFile("/home/zhipengwu/secureCRT/negtestinput.libsvm").map(parseLine)
+//    val parsed = sc.textFile("/home/zhipengwu/secureCRT/strictPosFeatureFile.libsvm").map(parseLine)
+//    val parsedNeg = sc.textFile("/home/zhipengwu/secureCRT/strictPosFeatureFile.libsvm").map(parseLine)
 
-
+    print("####开始计算cartesian")
     val pairs = parsed.cartesian(parsedNeg).filter(x => x._1._1 != x._2._1)
+    print("####结束计算cartesian")
 
 
-    val result = pairs.collect().toList;
 
-    println(result.toString)
+    //    val result = pairs.collect().toList;
+
+    //    println(result.toString)
 
     // Compute cosine similarity between pairs
-    val cosineSimilarity = pairs.map { case ((k1, m1), (k2, m2)) => ((k1, k2), sparseCosine(m1, m2)) }
-    val cosineResult = cosineSimilarity.collect().toList
+    val cosineSimilarity = pairs.map { case ((k1, m1), (k2, m2)) => ((k1, k2), sparseCosine(m1, m2)) }.filter(x=>x._2>0.1)
+
+    print("####结束计算cosine相似性")
+
+//    cosineSimilarity.saveAsTextFile("/home/zhipengwu/secureCRT/cosineSimilarityResult.txt")
+    //    val cosineResult = cosineSimilarity.collect().toList
+    val cosineResult = cosineSimilarity.take(20).toList
 
     println(cosineResult.toString())
 
@@ -72,7 +83,7 @@ object HelloTest {
 
     //            cosineSimilarity.write.format("json").save(saveResultPath)
 
-    println("hello")
+    println("############程序运行结束")
   }
 
 }
